4c4f090d73cf0ae9af42580c0794a9ce547b85ef
vanessaBonito <vanebonito@hotmail.com>
2022-08-10 12:36:38 -0500

    Update button's styles

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="btnNumber" type="button">
+        AC
+      </button>
+    );
+  }
+}
