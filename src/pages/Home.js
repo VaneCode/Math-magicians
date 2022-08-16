eef8e34e6222c9a57882fcdfdd1cb3a7d79fd658
vanessaBonito <vanebonito@hotmail.com>
2022-08-16 16:23:21 -0500

    Add Home component

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Home = () => (
+  <>
+    <section>
+      <h1>Welcome to our page!</h1>
+      <p>
+        Tincidunt tortor aliquam nulla facilisi cras fermentum odio.Nunc mattis
+        enim ut tellus elementum. Lacinia at quis risus sed vulputate. Eget
+        velit aliquet sagittis id. Eu scelerisque felis imperdiet proin
+        fermentum leo vel orci. Non consectetur a erat nam. Elit scelerisque
+        mauris pellentesque pulvinar pellentesque habitant.
+      </p>
+      <p>
+        Dignissim convallis aenean et tortor at risus viverra. Tellus mauris a
+        diam maecenas sed enim ut. Elit pellentesque habitant morbi tristique
+        senectus et netus. Ac auctor augue mauris augue neque gravida. Diam
+        volutpat commodo sed egestas. Purus viverra accumsan in nisl nisi
+        scelerisque eu ultrices vitae.
+      </p>
+    </section>
+  </>
+);
+
+export default Home;
