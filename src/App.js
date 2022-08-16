6d2b74d38547ad0acb4f3044ae71b4bb76d08ba9
vanessaBonito <vanebonito@hotmail.com>
2022-08-16 17:25:54 -0500

    Add routing with ReactRouting

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import CalculatorPage from './pages/CalculatorPage';
 import Quote from './pages/Quote';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<CalculatorPage />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </>
     );
   }
 }
