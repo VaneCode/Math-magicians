281f214a9431e29d2d0591a61d54fa961d015e2d
vanessaBonito <vanebonito@hotmail.com>
2022-08-16 16:40:17 -0500

    Add CalculatorPage component

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CalcUI from "../components/Calculator";
+
+const CalculatorPage = () =>{
+    return(
+        <section>
+            <h2>Let&apos;s do some math!</h2>
+            <CalcUI />
+        </section>
+    );
+}
+
+export default CalculatorPage;
