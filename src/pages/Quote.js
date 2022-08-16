3e96e00851eec3712121c9173bca480824faefcb
vanessaBonito <vanebonito@hotmail.com>
2022-08-16 16:33:16 -0500

    Add Quote component

---
+++ src/pages/Quote.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Quote = () => (
+  <div>
+    <p>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. -William Paul Thursdon American mathematician
+    </p>
+  </div>
+);
+
+export default Quote;
