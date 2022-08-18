7f0d8ba691f63cd8d1e140c92455e765cb02ca8a
vanessaBonito <vanebonito@hotmail.com>
2022-08-17 20:05:50 -0500

    Fix eslint errors

--- src/_test_/Navbar.test.js
+++ src/_test_/Navbar.test.js
@@ -1,14 +1,14 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
 
 describe('Testing component Navbar', () => {
   test('Render Navbar', () => {
     const tree = render(
-        <Router>
-          <Navbar />
-        </Router>,
-      );
+      <Router>
+        <Navbar />
+      </Router>,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
