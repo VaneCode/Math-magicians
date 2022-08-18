deea216a154f1fc04250eb8eaa2bf7e25cf9d45d
vanessaBonito <vanebonito@hotmail.com>
2022-08-17 20:03:51 -0500

    Add test to Navbar

---
+++ src/_test_/Navbar.test.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../components/Navbar'
+
+describe('Testing component Navbar', () => {
+  test('Render Navbar', () => {
+    const tree = render(
+        <Router>
+          <Navbar />
+        </Router>,
+      );
+    expect(tree).toMatchSnapshot();
+  });
+});
